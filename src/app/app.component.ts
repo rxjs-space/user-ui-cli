4db4bb0efab5ef991ef5478e893a5ce765a2283b
Liu Tim <rxjs-space@outlook.com>
2017-01-13 20:28:17 +0800

    installs bootstrap4-a6 and switches to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ let md = new MarkdownIt();
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
