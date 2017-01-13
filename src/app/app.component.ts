1948e2f0e9e534cacf49db8d7ac76b45022d7382
Liu Tim <rxjs-space@outlook.com>
2017-01-13 22:00:41 +0800

    plays with typedoc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import * as MarkdownIt from 'markdown-it';
-let md = new MarkdownIt();
+/**
+ * use `md.render(markdown in rawtext)` to generate html
+ * out of markdown, for example:
+ * ```
+ * const htmlFromMarkdown_text = md.render('# gogogo');
+ * const htmlFromMarkdown_file = md.render(require('raw-loader!./app.md));
+ * ```
+ */
+const md = new MarkdownIt();
 
+/**
+ * root component for the app
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  /**
+   * title for foo.bar.john.doe
+   */
   title = 'app works!';
-  mdContent = md.render(require('raw-loader!./app.md'));
+  /**
+   * holds the html generated from app.md markdown file
+   */
+  htmlFromMarkdown = md.render(require('raw-loader!./app.md'));
 }
+
