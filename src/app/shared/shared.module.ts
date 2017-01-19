c21199ba8d4a3dd007246b5fbeb0e4632da99d35
Liu Tim <rxjs-space@outlook.com>
2017-01-19 14:33:37 +0800

    clears up ArticlesApi

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { SharedComponent } from './shared.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 import { ThreadApi, BookApi, ShowcaseApi,
-  ColumnsApi, ArticlesApi, AuthorsApi } from './api';
+  ColumnsApi, AuthorsApi } from './api';
   import { ApiService } from './api/api.service';
 import { AuthService, SvgHolderService, FetchGithubService } from './services';
 import { LayoutNavComponent } from './component/layout-nav/layout-nav.component';
@@ -37,7 +37,7 @@ export class SharedModule {
       providers: [
         AuthService, SvgHolderService, FetchGithubService,
         ThreadApi, BookApi, ShowcaseApi, ApiService, ColumnsApi,
-        ArticlesApi, AuthorsApi
+        AuthorsApi
       ]
     }
   }
