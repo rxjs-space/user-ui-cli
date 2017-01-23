1de844f2c52940baad6133db6ffc163c909cae99
Liu Tim <rxjs-space@outlook.com>
2017-01-23 12:40:21 +0800

    moves polyfills to angular-cli.json

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@
 
 // platformBrowserDynamic().bootstrapModule(AppModule);
 
-import './polyfills.ts';
+// import './polyfills.ts';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
